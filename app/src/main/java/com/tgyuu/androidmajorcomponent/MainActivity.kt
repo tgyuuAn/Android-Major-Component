d3773056d72c487f3fbee5375c7b8fa28f816ae1
tgyuuAn <atg0614@gmail.com>
2025-04-16 17:13:26 +0900

    chore: 불필요한 코드 제거

--- app/src/main/java/com/tgyuu/androidmajorcomponent/MainActivity.kt
+++ app/src/main/java/com/tgyuu/androidmajorcomponent/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.tgyuu.androidmajorcomponent
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -21,7 +21,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidMajorComponentTheme {
-                val calendarState = remember { CalendarState() }
+                val calendarState = remember {
+                    CalendarState(
+                        onDateSelectCallBack = { selectedDate ->
+                            Log.d("test", selectedDate.toString())
+                        }
+                    )
+                }
 
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column(modifier = Modifier.padding(innerPadding)) {
--- app/src/main/java/com/tgyuu/androidmajorcomponent/ui/component/calendar/CalendarState.kt
+++ app/src/main/java/com/tgyuu/androidmajorcomponent/ui/component/calendar/CalendarState.kt
@@ -1,6 +1,5 @@
 package com.tgyuu.androidmajorcomponent.ui.component.calendar
 
-import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -17,16 +16,13 @@ class CalendarState(
         selectedDate = date
         currentDisplayDate = date
         onDateSelectCallBack(date)
-        Log.d("test", "onDateSelect 호출 $selectedDate")
     }
 
     fun onNextMonthClick() {
         currentDisplayDate = currentDisplayDate.plusMonths(1)
-        Log.d("test", "onNextMonthClick 호출 $currentDisplayDate")
     }
 
     fun onPreviousMonthClick() {
         currentDisplayDate = currentDisplayDate.minusMonths(1)
-        Log.d("test", "onPreviousMonthClick 호출 $currentDisplayDate")
     }
 }
