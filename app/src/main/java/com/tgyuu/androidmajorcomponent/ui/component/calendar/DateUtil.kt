f969db41a3c9f4e673bbb498ef4afa8cd3a53057
tgyuuAn <atg0614@gmail.com>
2025-04-16 14:50:04 +0900

    refactor : 리팩터링 이후 1일의 요일을 구하는 메서드 가시성 수정 및 새롭게 작성된 테스트로 인해 확신을 주는 테스트 코드 제거

--- app/src/main/java/com/tgyuu/androidmajorcomponent/ui/component/calendar/DateUtil.kt
+++ app/src/main/java/com/tgyuu/androidmajorcomponent/ui/component/calendar/DateUtil.kt
@@ -13,7 +13,10 @@ fun DayOfWeek.toKorean(): String = when (this) {
     DayOfWeek.SUNDAY -> "일"
 }
 
-fun getFirstDayOfWeek(date: LocalDate): DayOfWeek {
+/**
+ * 해당 날짜의 달에 1일의 요일을 구합니다.
+ */
+private fun getFirstDayOfWeek(date: LocalDate): DayOfWeek {
     return date.withDayOfMonth(1).dayOfWeek
 }
 
@@ -21,8 +24,13 @@ fun getFirstDayOfWeek(date: LocalDate): DayOfWeek {
  * 해당 월의 달력을 6줄 7칸 기준으로 그릴 때,
  * 1일 전에 보여야 할 이전 달의 요일 목록을 반환합니다.
  */
-fun getPreviousMonthDaysToShow(date: LocalDate): List<DayOfWeek> = listOf(
-    DayOfWeek.MONDAY,
-    DayOfWeek.TUESDAY,
-    DayOfWeek.WEDNESDAY
-)
+fun getPreviousMonthDaysToShow(date: LocalDate): List<DayOfWeek> {
+    val firstDayOfMonth = date.withDayOfMonth(1).dayOfWeek
+    val count = if (firstDayOfMonth == DayOfWeek.SUNDAY) {
+        7
+    } else {
+        firstDayOfMonth.value - 1
+    }
+
+    return (1..count).map { DayOfWeek.of(it) }
+}
--- app/src/test/java/com/tgyuu/androidmajorcomponent/ui/component/calendar/DateUtilTest.kt
+++ app/src/test/java/com/tgyuu/androidmajorcomponent/ui/component/calendar/DateUtilTest.kt
@@ -9,15 +9,6 @@ class DateUtilTest : BehaviorSpec({
     given("특정 날짜가 주어졌을 때") {
         val givenDate = LocalDate.of(2025, 5, 16)
 
-        `when`("그 달의 1일의 요일을 구해서") {
-            val actual = getFirstDayOfWeek(givenDate)
-
-            then("반환할 수 있다.") {
-                val expected = DayOfWeek.THURSDAY  // 2025-05-01은 목요일
-                actual shouldBe expected
-            }
-        }
-
         `when`("달력에서 이전 달이 보이는 월요일부터 1일의 요일까지 구해서") {
             val actual = getPreviousMonthDaysToShow(givenDate)
 
