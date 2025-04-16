8b16d5491b54369e5fa9ff7f112d1c3f4de599a3
tgyuuAn <atg0614@gmail.com>
2025-04-16 14:13:43 +0900

    feat : 달력 Body 구현

--- app/src/main/java/com/tgyuu/androidmajorcomponent/ui/component/calendar/Calendar.kt
+++ app/src/main/java/com/tgyuu/androidmajorcomponent/ui/component/calendar/Calendar.kt
@@ -1,24 +1,55 @@
 package com.tgyuu.androidmajorcomponent.ui.component.calendar
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.dp
 import java.time.LocalDate
 
 @Composable
 fun NormalCalendar(
     calendarState: CalendarState,
+    modifier: Modifier = Modifier,
 ) {
-    Column {
+    Column(modifier) {
         CalendarHeader(currentDate = calendarState.currentMonthDate)
+        CalendarBody()
     }
 }
 
 @Composable
 private fun CalendarHeader(
     currentDate: LocalDate,
+    modifier: Modifier = Modifier,
 ) {
     Text(
-        text = "${currentDate.year}년 ${currentDate.monthValue}월"
+        text = "${currentDate.year}년 ${currentDate.monthValue}월",
+        modifier = modifier.semantics { contentDescription = "달력 헤더" },
     )
+}
+
+@Composable
+private fun CalendarBody(
+    modifier: Modifier = Modifier,
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(7),
+        modifier = modifier.semantics { contentDescription = "달력 바디" },
+    ) {
+        item {
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(10.dp),
+            )
+        }
+    }
 }
