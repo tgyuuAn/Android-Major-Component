f57d5928af2fac1fa256bfabba498a1765fd96b4
tgyuuAn <atg0614@gmail.com>
2025-04-16 14:38:24 +0900

    test : 선택된 날짜의 1일의 요일을 구하는 테스트 코드 작성

---
+++ app/src/test/java/com/tgyuu/androidmajorcomponent/ui/component/calendar/DateUtilTest.kt
@@ -0,0 +1,20 @@
+package com.tgyuu.androidmajorcomponent.ui.component.calendar
+
+import io.kotest.core.spec.style.BehaviorSpec
+import java.time.DayOfWeek
+import java.time.LocalDate
+
+class DateUtilTest : BehaviorSpec({
+    given("날짜가 주어졌을 때") {
+        val givenDate = LocalDate.of(2025, 4, 16)
+
+        `when`("그 달의 1일의 요일을 구해서") {
+            val actual = getFirstDayOfWeek(givenDate)
+
+            then("반환할 수 있다.") {
+                val expected = DayOfWeek.TUESDAY  // 2025-04-01은 화요일
+                actual shouldBe expected
+            }
+        }
+    }
+})
