ebe981d25fe7e37b562fd45591657332b1cf073c
tgyuuAn <atg0614@gmail.com>
2025-04-16 14:44:50 +0900

    test : 6*7 달력에서 이전 달이 보이는 요일 목록을 반환하는 테스트 작성

--- app/src/test/java/com/tgyuu/androidmajorcomponent/ui/component/calendar/DateUtilTest.kt
+++ app/src/test/java/com/tgyuu/androidmajorcomponent/ui/component/calendar/DateUtilTest.kt
@@ -6,14 +6,28 @@ import java.time.DayOfWeek
 import java.time.LocalDate
 
 class DateUtilTest : BehaviorSpec({
-    given("날짜가 주어졌을 때") {
-        val givenDate = LocalDate.of(2025, 4, 16)
+    given("특정 날짜가 주어졌을 때") {
+        val givenDate = LocalDate.of(2025, 5, 16)
 
         `when`("그 달의 1일의 요일을 구해서") {
             val actual = getFirstDayOfWeek(givenDate)
 
             then("반환할 수 있다.") {
-                val expected = DayOfWeek.TUESDAY  // 2025-04-01은 화요일
+                val expected = DayOfWeek.THURSDAY  // 2025-05-01은 목요일
+                actual shouldBe expected
+            }
+        }
+
+        `when`("달력에서 이전 달이 보이는 월요일부터 1일의 요일까지 구해서") {
+            val actual = getPreviousMonthDaysToShow(givenDate)
+
+            then("반환할 수 있다.") {
+                // 2025-05-01은 목요일, 이전 달 요일에 월 화, 수가 보임
+                val expected = listOf(
+                    DayOfWeek.MONDAY,
+                    DayOfWeek.TUESDAY,
+                    DayOfWeek.WEDNESDAY,
+                )
                 actual shouldBe expected
             }
         }
