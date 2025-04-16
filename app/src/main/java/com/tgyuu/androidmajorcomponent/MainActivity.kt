2241eea1b60f811161e3f673a0fecc2823e56ffb
tgyuuAn <atg0614@gmail.com>
2025-04-16 18:06:22 +0900

    chore : 예시 코드 추가

--- app/src/main/java/com/tgyuu/androidmajorcomponent/MainActivity.kt
+++ app/src/main/java/com/tgyuu/androidmajorcomponent/MainActivity.kt
@@ -5,11 +5,14 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.tgyuu.androidmajorcomponent.ui.component.calendar.NormalCalendar
 import com.tgyuu.androidmajorcomponent.ui.component.calendar.SwipeableCalendar
 import com.tgyuu.androidmajorcomponent.ui.component.calendar.core.rememberCalendarState
 import com.tgyuu.androidmajorcomponent.ui.foundation.AndroidMajorComponentTheme
@@ -21,13 +24,22 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             AndroidMajorComponentTheme {
-                val calendarState = rememberCalendarState()
+                val normalCalendarState = rememberCalendarState()
+                val swipeableCalendarState = rememberCalendarState()
 
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.padding(innerPadding)) {
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(30.dp),
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        NormalCalendar(
+                            calendarState = normalCalendarState,
+                            onDateSelect = { Log.d("NormalCalendar", "selectedDate : $it") },
+                        )
+
                         SwipeableCalendar(
-                            calendarState = calendarState,
-                            onDateSelect = { Log.d("test", it.toString()) },
+                            calendarState = swipeableCalendarState,
+                            onDateSelect = { Log.d("SwipeableCalendar", "selectedDate : $it") },
                         )
                     }
                 }
